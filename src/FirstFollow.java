338763fc627ee11f1f4ceee70aedc4c086c775ac
Muqarrab <mickey32717@gmail.com>
2026-03-23 23:08:03 +0500

    First Follow sets

--- src/FirstFollow.java
+++ src/FirstFollow.java
@@ -5,69 +5,164 @@ public class FirstFollow {
     public ArrayList<ArrayList<String>> followSets = new ArrayList<>();
     private Grammar grammar;
 
-    public FirstFollow(Grammar g) {
+    public FirstFollow(Grammar g) 
+    {
         this.grammar = g;
     }
 
     public void computeFirstSets() {
-        for (int i = 0; i < grammar.nonTerminals.size(); i++) firstSets.add(new ArrayList<>());
+        // empty list for non terminal
+        for (int i = 0; i < grammar.nonTerminals.size(); i++) 
+        {
+            firstSets.add(new ArrayList<>());
+        }
 
         boolean changed = true;
-        while (changed) {
+        while (changed) 
+            {
             changed = false;
-            for (int i = 0; i < grammar.nonTerminals.size(); i++) {
+            for (int i = 0; i < grammar.nonTerminals.size(); i++) 
+                {
                 int beforeSize = firstSets.get(i).size();
-                for (ArrayList<String> production : grammar.allRules.get(i)) {
+                ArrayList<ArrayList<String>> currentRules = grammar.allRules.get(i);
+
+                for (int j = 0; j < currentRules.size(); j++) 
+                    {
+                    ArrayList<String> production = currentRules.get(j);
+                    
+                    // epsilon rule
                     if (production.size() == 1 && (production.get(0).equals("epsilon") || production.get(0).equals("@"))) {
                         addUnique(firstSets.get(i), "epsilon");
                         continue;
                     }
+
+                    // rule 2
                     boolean allDeriveEpsilon = true;
-                    for (String symbol : production) {
+                    for (int k = 0; k < production.size(); k++) {
+                        String symbol = production.get(k);
+
                         if (!grammar.isNonTerminal(symbol)) {
+                            // then terminal hai
                             addUnique(firstSets.get(i), symbol);
                             allDeriveEpsilon = false;
                             break;
                         } else {
+                            // add first set if epsilon
                             int ntIndex = grammar.findNonTerminalIndex(symbol);
-                            ArrayList<String> nextFirst = firstSets.get(ntIndex);
+                            ArrayList<String> symbolFirst = firstSets.get(ntIndex);
+                            
                             boolean symbolHasEpsilon = false;
-                            for (String s : nextFirst) {
-                                if (!s.equals("epsilon")) addUnique(firstSets.get(i), s);
-                                else symbolHasEpsilon = true;
+                            for (int m = 0; m < symbolFirst.size(); m++) 
+                                {
+                                String s = symbolFirst.get(m);
+                                if (!s.equals("epsilon")) 
+                                {
+                                    addUnique(firstSets.get(i), s);
+                                } else 
+                                {
+                                    symbolHasEpsilon = true;
+                                }
                             }
-                            if (!symbolHasEpsilon) { allDeriveEpsilon = false; break; }
+                            
+                            
+                            if (!symbolHasEpsilon) 
+                            {
+                                allDeriveEpsilon = false;
+                                break;
+                            }
                         }
                     }
-                    if (allDeriveEpsilon) addUnique(firstSets.get(i), "epsilon");
+                    if (allDeriveEpsilon) 
+                    {
+                        addUnique(firstSets.get(i), "epsilon");
+                    }
                 }
-                if (firstSets.get(i).size() != beforeSize) changed = true;
+                if (firstSets.get(i).size() != beforeSize) 
+                    changed = true;
             }
         }
     }
 
-    public void computeFollowSets() {
-        for (int i = 0; i < grammar.nonTerminals.size(); i++) followSets.add(new ArrayList<>());
-        if (!grammar.nonTerminals.isEmpty()) addUnique(followSets.get(0), "$");
+    public void computeFollowSets() 
+    {
+
+        for (int i = 0; i < grammar.nonTerminals.size(); i++) {
+            followSets.add(new ArrayList<>());
+        }
+
+
+        if (grammar.nonTerminals.size() > 0) {
+            addUnique(followSets.get(0), "$");
+        }
 
         boolean changed = true;
-        while (changed) {
+        while (changed) 
+            {
             changed = false;
-            for (int i = 0; i < grammar.nonTerminals.size(); i++) {
-                for (ArrayList<String> production : grammar.allRules.get(i)) {
-                    for (int j = 0; j < production.size(); j++) {
-                        String B = production.get(j);
+            for (int i = 0; i < grammar.nonTerminals.size(); i++) 
+                {
+                ArrayList<ArrayList<String>> rules = grammar.allRules.get(i);
+
+                for (int j = 0; j < rules.size(); j++) {
+                    ArrayList<String> production = rules.get(j);
+
+                    
+                    for (int k = 0; k < production.size(); k++) {
+                        String B = production.get(k);
+
                         if (grammar.isNonTerminal(B)) {
                             int bIndex = grammar.findNonTerminalIndex(B);
                             int beforeSize = followSets.get(bIndex).size();
-                            if (j + 1 < production.size()) {
-                                ArrayList<String> beta = new ArrayList<>(production.subList(j + 1, production.size()));
-                                ArrayList<String> firstBeta = getFirstOfSequence(beta);
-                                for (String s : firstBeta) if (!s.equals("epsilon")) addUnique(followSets.get(bIndex), s);
-                                if (firstBeta.contains("epsilon")) for (String s : followSets.get(i)) addUnique(followSets.get(bIndex), s);
-                            } else {
-                                for (String s : followSets.get(i)) addUnique(followSets.get(bIndex), s);
+
+                            
+                            boolean allSuffixDeriveEpsilon = true;
+                            for (int next = k + 1; next < production.size(); next++) 
+                                {
+                                String nextSymbol = production.get(next);
+
+                                if (!grammar.isNonTerminal(nextSymbol)) 
+                                    {
+                                    
+                                    addUnique(followSets.get(bIndex), nextSymbol);
+                                    allSuffixDeriveEpsilon = false;
+                                    break;
+                                } else 
+                                    {
+                                    
+                                    int nextNtIdx = grammar.findNonTerminalIndex(nextSymbol);
+                                    ArrayList<String> nextFirst = firstSets.get(nextNtIdx);
+                                    
+                                    boolean nextCanBeEmpty = false;
+                                    for (int m = 0; m < nextFirst.size(); m++) 
+                                        {
+                                        String s = nextFirst.get(m);
+                                        if (!s.equals("epsilon")) 
+                                        {
+                                            addUnique(followSets.get(bIndex), s);
+                                        } else 
+                                        {
+                                            nextCanBeEmpty = true;
+                                        }
+                                    }
+                                    
+                                    if (!nextCanBeEmpty)
+                                    {
+                                        allSuffixDeriveEpsilon = false;
+                                        break;
+                                    }
+                                }
                             }
+
+
+                            if (allSuffixDeriveEpsilon) 
+                                {
+                                ArrayList<String> followA = followSets.get(i);
+                                for (int m = 0; m < followA.size(); m++) 
+                                {
+                                    addUnique(followSets.get(bIndex), followA.get(m));
+                                }
+                            }
+
                             if (followSets.get(bIndex).size() != beforeSize) changed = true;
                         }
                     }
@@ -76,44 +171,77 @@ public class FirstFollow {
         }
     }
 
-    public ArrayList<String> getFirstOfSequence(ArrayList<String> sequence) {
+    
+    public ArrayList<String> getFirstOfSequence(ArrayList<String> sequence) 
+    {
         ArrayList<String> result = new ArrayList<>();
-        boolean allDeriveEpsilon = true;
-        for (String symbol : sequence) {
-            if (!grammar.isNonTerminal(symbol)) {
-                addUnique(result, symbol);
-                allDeriveEpsilon = false;
+        boolean allEmpty = true;
+        for (int i = 0; i < sequence.size(); i++) 
+            {
+            String s = sequence.get(i);
+            if (!grammar.isNonTerminal(s)) 
+            {
+                addUnique(result, s);
+                allEmpty = false;
                 break;
-            } else {
-                int ntIndex = grammar.findNonTerminalIndex(symbol);
-                ArrayList<String> firstOfSymbol = firstSets.get(ntIndex);
-                boolean hasEpsilon = false;
-                for (String s : firstOfSymbol) {
-                    if (s.equals("epsilon")) hasEpsilon = true;
-                    else addUnique(result, s);
+            } 
+            else 
+            {
+                int idx = grammar.findNonTerminalIndex(s);
+                ArrayList<String> f = firstSets.get(idx);
+                boolean hasEps = false;
+                for (int j = 0; j < f.size(); j++) {
+                    if (f.get(j).equals("epsilon")) hasEps = true;
+                    else addUnique(result, f.get(j));
                 }
-                if (!hasEpsilon) { allDeriveEpsilon = false; break; }
+                if (!hasEps) 
+                {
+                    allEmpty = false;
+                    break;
+                }
             }
         }
-        if (allDeriveEpsilon) addUnique(result, "epsilon");
+        if (allEmpty) addUnique(result, "epsilon");
         return result;
     }
 
     private void addUnique(ArrayList<String> list, String val) {
-        if (!list.contains(val)) list.add(val);
+        boolean found = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(val)) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) list.add(val);
     }
 
     public void displayFirstSets() {
-        System.out.println("\n--- FIRST Sets ---");
-        for (int i = 0; i < grammar.nonTerminals.size(); i++) {
-            System.out.println("FIRST(" + grammar.nonTerminals.get(i) + ") = { " + String.join(", ", firstSets.get(i)) + " }");
+        System.out.println("\nFIRST Sets");
+        for (int i = 0; i < grammar.nonTerminals.size(); i++) 
+            {
+            System.out.print("FIRST(" + grammar.nonTerminals.get(i) + ") = { ");
+            ArrayList<String> set = firstSets.get(i);
+            for (int j = 0; j < set.size(); j++) 
+            {
+                System.out.print(set.get(j) + (j < set.size() - 1 ? ", " : ""));
+            }
+            System.out.println(" }");
         }
     }
 
-    public void displayFollowSets() {
-        System.out.println("\n--- FOLLOW Sets ---");
-        for (int i = 0; i < grammar.nonTerminals.size(); i++) {
-            System.out.println("FOLLOW(" + grammar.nonTerminals.get(i) + ") = { " + String.join(", ", followSets.get(i)) + " }");
+    public void displayFollowSets() 
+    {
+        System.out.println("\nFOLLOW Sets");
+        for (int i = 0; i < grammar.nonTerminals.size(); i++) 
+            {
+            System.out.print("FOLLOW(" + grammar.nonTerminals.get(i) + ") = { ");
+            ArrayList<String> set = followSets.get(i);
+            for (int j = 0; j < set.size(); j++) 
+            {
+                System.out.print(set.get(j) + (j < set.size() - 1 ? ", " : ""));
+            }
+            System.out.println(" }");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Grammar grammar = new Grammar();
         try {
-            // 1. Load and Transform
+            // this will load from file
             grammar.loadFromFile("input/grammar1.txt");
             grammar.applyLeftFactoring();
             grammar.removeLeftRecursion();
             grammar.display();
             
-            // 2. Compute Sets using the modular FirstFollow class
+            // first and follow sets
             FirstFollow ff = new FirstFollow(grammar);
             ff.computeFirstSets();
             ff.displayFirstSets();
@@ -22,16 +22,18 @@ public class Main {
             parser.buildParsingTable();
             parser.displayTable();
 
-            // Check if we can proceed to Part 2
-            if (!parser.isLL1) {
-                System.out.println("Note: Because there are conflicts, the parser might pick the first available rule.");
+            
+            if (!parser.isLL1) 
+            {
+                System.out.println("Conflict, picks first available rule");
             }
             
             System.out.println("\nNon-Terminals: " + grammar.nonTerminals);
             System.out.println("Terminals: " + grammar.terminals);
             
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
+        } catch (IOException e) 
+        {
+            System.err.println("Error reading file ");
         }
     }
 }
